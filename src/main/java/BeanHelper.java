01e1017a92045d3be7c0bf3c119a47dfa6823810
niuchao <niu_chao@suixingpay.com>
2018-03-20 01:01:06 +0800

    实现IOC

---
+++ src/main/java/BeanHelper.java
@@ -0,0 +1,42 @@
+
+import Util.ReflectionUtil;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+
+public class BeanHelper {
+    /**
+     * 定义bean映射，（用于存放bean类与bean实例的映射关系）
+     */
+    private static final Map<Class<?>,Object> BEAN_MAP = new HashMap<Class<?>, Object>();
+
+    /**
+     * 静态代码块
+     */
+    static {
+        Set<Class<?>> beanClassSet = ClassHelper.getBeanClassSet();
+        for (Class<?> beanClass : beanClassSet) {
+            Object object = ReflectionUtil.newInstance(beanClass);
+            BEAN_MAP.put(beanClass,object);
+        }
+    }
+
+    /**
+     * 获取Bean映射
+     */
+    public static Map<Class<?>,Object> getBeanMap(){
+        return BEAN_MAP;
+    }
+
+    /**
+     * 获取Bean实例
+     */
+    public static <T> T getBean(Class<?> cls){
+        if (!BEAN_MAP.containsKey(cls)){
+            throw new RuntimeException("can not get bean by class:"+cls);
+        }
+        return (T) BEAN_MAP.get(cls);
+    }
+}
---
+++ src/main/java/IocHelper.java
@@ -0,0 +1,42 @@
+
+import Util.ArrayUtil;
+import Util.CollectionUtil;
+import Util.ReflectionUtil;
+import annotation.Inject;
+
+import java.lang.reflect.Field;
+import java.util.Map;
+
+public class IocHelper {
+    static {
+        //获取所有的Bean类与Bean实例之间的关系（简称Bean Map）
+        Map<Class<?>, Object> beanMap = BeanHelper.getBeanMap();
+        if (CollectionUtil.isNotEmpty(beanMap)) {
+            //遍历beanMap
+            for (Map.Entry<Class<?>, Object> beanEntry : beanMap.entrySet()) {
+                //从beanMap中获取bean类与bean实例
+                Class<?> beanClass = beanEntry.getKey();
+                Object beanInstance = beanEntry.getValue();
+                //获取Bean类定义的所有成员变量（简称Bean Field）
+                Field [] beanFields = beanClass.getDeclaredFields();
+                if (ArrayUtil.isNotEmpty(beanFields)){
+                    //遍历beanField
+                    for (Field beanField : beanFields) {
+                        //判断当前的Bean Field是否带有Inject注解
+                        if (beanField.isAnnotationPresent(Inject.class)){
+                            //在Bean Map中获取Bean Field对应的实例
+                            Class<?> beanFieldClass = beanField.getType();
+                            Object beanFieldInstance = beanMap.get(beanFieldClass);
+                            if (beanFieldInstance != null){
+                                //通过放射初始化beanField值
+                                ReflectionUtil.setField(beanInstance,beanField,beanFieldInstance);
+                            }
+                        }
+                    }
+                }
+
+
+            }
+        }
+    }
+}
